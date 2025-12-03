faf265117592716ad18f18e280b11bed5ea541dc
vlad <95431727+Vlad1-1@users.noreply.github.com>
2025-12-03 12:10:47 +0200

    Slight refactor day 2

--- solutions/aockt/y2025/Y2025D02.kt
+++ solutions/aockt/y2025/Y2025D02.kt
@@ -3,33 +3,30 @@ package aockt.y2025
 import io.github.jadarma.aockt.core.Solution
 
 object Y2025D02 : Solution {
-    fun parseInput(input: String) = input.split(',').map {
-        val numbers = it.split('-')
-        assert(numbers.size == 2)
-        numbers[0].toULong()..numbers[1].toULong()
-    }
+    private fun parseInput(input: String) =
+        input.split(',').map {
+            val numbers = it.split('-')
+            assert(numbers.size == 2)
+            numbers[0].toULong()..numbers[1].toULong()
+        }
 
     override fun partOne(input: String) =
-        parseInput(input).flatMap { range ->
-            range.filter {
-                val str = it.toString()
-                val half = str.length / 2
-                str.take(half) == str.substring(half)
-            }
+        parseInput(input).flatten().filter {
+            val str = it.toString()
+            val half = str.length / 2
+            str.take(half) == str.substring(half)
         }.sum()
 
     override fun partTwo(input: String) =
-        parseInput(input).flatMap { range ->
-            range.filter {
-                val str = it.toString()
-                val len = str.length
-                for (i in len - len / 2 until len) {
-                    if (len % (len - i) != 0) continue
-                    if (str.take(i) == str.substring(len - i)) {
-                        return@filter true
-                    }
+        parseInput(input).flatten().filter {
+            val str = it.toString()
+            val len = str.length
+            for (i in len - len / 2 until len) {
+                if (len % (len - i) != 0) continue
+                if (str.take(i) == str.substring(len - i)) {
+                    return@filter true
                 }
-                return@filter false
             }
+            false
         }.sum()
 }
